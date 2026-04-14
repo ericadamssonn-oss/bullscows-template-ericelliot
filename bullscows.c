df252a9e7002bdff238360c300416a309c5f8802
ellio <elliotmoldberg@gmail.com>
2026-04-14 11:42:41 +0200

    Merge remote-tracking branch 'origin/main'

--- bullscows.c
+++ bullscows.c
@@ -105,10 +105,10 @@ int check_guess_format(int guess) {
         size++;
     }
     if (size == 4) {
-        int digit4 =(gues) % 10;
+        int digit4 =(guess) % 10;
         int digit3 = (guess/10) % 10;
-        int digit2 = (guess/100) %10
-        int digit1 = (guess/1000) %10
+        int digit2 = (guess/100) %10;
+        int digit1 = (guess/1000) %10;
         // kollar så talen är mellan 1-9
         if (digit1>= 1 && digit1<=9 && digit2>= 1 && digit2<=9 && digit3>= 1 && digit3<=9 && digit4>= 1 && digit4<=9){
             // kolla efter dubbleter
@@ -117,7 +117,8 @@ int check_guess_format(int guess) {
             }
         }
     }
-    printf("error")// error fel inmatning
+
+    printf("error");// error fel inmatning
     return -2;
 
 }
@@ -134,64 +135,5 @@ void print_instructions() {
 // *********************************************************
 
 int main(void) {
-    setbuf(stdout, 0);
-    srand(time(NULL));
 
-    if (TEST) {
-        test();
-    }
-
-    int answer = get_random_4digit();
-
-    if (DEBUG) {
-        printf("Answer is %d\n", answer);
-    }
-
-    print_instructions();
-
-    bool aborted = false;
-    bool guessed = false;
-    int number_of_guesses = 0;
-    int current_guess = 0;
-    int bulls = 0;
-    int cows = 0;
-
-    do {
-        current_guess = get_player_guess();
-
-        if (current_guess == -1) {
-            aborted = true;
-        } else {
-            number_of_guesses++;
-            bulls = count_bulls(current_guess, answer);
-            int total = count_cows_and_bulls(current_guess, answer);
-            cows = total - bulls;
-
-            printf("Bulls: %d, Cows: %d\n", bulls, cows);
-
-            if (bulls == 4) {
-                guessed = true;
-            }
-        }
-
-    } while (!guessed && !aborted);
-
-    if (aborted) {
-        printf("Game aborted\n");
-    } else {
-        printf("Done, number was %d you needed %d guess(es)\n", answer, number_of_guesses);
-    }
-    return 0;
-}
-
-// --- Test-funktioner och implementationer behålls i slutet ---
-void assert(bool v1, bool v2) {
-    if (v1 == v2) printf("Test passed\n");
-    else printf("Test failed\n");
-}
-
-void test() {
-    printf("running tests\n");
-    // Här kan du avkommentera dina tester när du vill köra dem
-    exit(0);
 }
